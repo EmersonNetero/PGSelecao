07b23ce196de6d85ef7d99d7ca9d4e5949d31f3c
Emersumm <emersonbk15@gmail.com>
2022-01-14 18:18:02 -0300

    back end em desenvolvimento

---
+++ server/src/routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+import HelloController from './controllers/HelloController.js'
+
+const routes = new Router()
+
+routes.get("/hello", HelloController.index)
+
+
+export default routes;
